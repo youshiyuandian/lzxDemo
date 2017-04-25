ecf302deb539cc52ce9abd42b79b12f84b964973
zuxi li <ios_lizuxi@163.com>
2017-04-25 11:18:35 +0800

    完

---
+++ Demo/Demo/LZXPerson/Person.h
@@ -0,0 +1,14 @@
+//
+//  Person.h
+//  Demo
+//
+//  Created by zuxi li on 2017/4/25.
+//  Copyright © 2017年 zuxi li. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
++ (void)eat;
+@end
---
+++ Demo/Demo/LZXPerson/Person.m
@@ -0,0 +1,15 @@
+//
+//  Person.m
+//  Demo
+//
+//  Created by zuxi li on 2017/4/25.
+//  Copyright © 2017年 zuxi li. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
++ (void)eat {
+    NSLog(@"person eating");
+}
+@end
